d3eb75376bdb8d4180dab2df4818be8217fbb7c0
Payal Tikyani <payal.tikyani@ongraph.com>
2024-05-21 15:34:44 +0530

    resolved comment

--- src/main/java/com/notification/config/TripplyBookingConfig.java
+++ src/main/java/com/notification/config/TripplyBookingConfig.java
@@ -5,15 +5,22 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.List;
 
 @Configurable
 public class TripplyBookingConfig {
 
+    @Value("${application.notification.base-url}")
+    private String url;
+
+    @Value("${spring.application.version}")
+    private String version;
+
     public OpenAPI defineOpenApi(){
         Server server = new Server();
-        server.setUrl("http://localhost:8087");
+        server.setUrl(url);
         server.setDescription("Development");
 
         Contact contact = new Contact();
@@ -22,7 +29,7 @@ public class TripplyBookingConfig {
 
         Info information = new Info()
                 .title("Tripply - Hotel Booking System")
-                .version("1.0")
+                .version(version)
                 .description("This API exposes endpoints for notification service")
                 .contact(contact);
         return new OpenAPI().info(information).servers(List.of(server));
